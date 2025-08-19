44a84c581d8acf3d0d02144abb3aa9a5c8268736
Rifadul Islam Rifat <rifadukrifat@gmail.com>
2025-08-20 02:10:26 +0600

    updated files

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables
+load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
+database_url = os.getenv("database_url")
 print(API_KEY)
+print(database_url)
